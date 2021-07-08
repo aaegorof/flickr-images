4f19092e802c8b5ae97739f937a82f00fce6cd39
Artemiy <aaegorof@gmail.com>
2021-07-08 18:33:33 +0300

    Seems to conquer the library...

--- src/components/ImageEdges/ImageEdges.js
+++ src/components/ImageEdges/ImageEdges.js
@@ -8,60 +8,60 @@ import './imgeEdges.scss'
 const ImageEdges = (props) => {
   const [cv, setCv] = useState()
   const [imageInfo, setImageInfo] = useState()
+  const [imgUrl, setImgUrl] = useState('')
+  const [size, setSize] = useState([0,0])
+
   const { id, secret } = useParams()
+
   const canvasRef = useRef(null)
-  // const canvasOutput = useRef(null) as MutableRefObject<any>
+  const canvasOutput = useRef(null)
+  const originalImg = useRef(null)
 
   const history = useHistory()
 
   useEffect(() => {
     getImage(id, secret).then((res) => {
-      setImageInfo(res.data.photo)
+      const imageInfo = res.data.photo
+      setImageInfo(imageInfo)
+      setImgUrl(`${CONSTRUCT_BASE_URL}${imageInfo?.server}/${imageInfo?.id}_${imageInfo?.secret}.jpg`)
     })
   }, [id, secret])
-  
+
   useEffect(() => {
     if (canvasRef.current) {
-      // const canvas = canvasRef.current
-      // const ctx = canvas.getContext('2d')
-      //
-      // let base_image = new Image();
-      // console.log('imageInfo',imageInfo)
-      // base_image.src = `${CONSTRUCT_BASE_URL}${imageInfo?.server}/${imageInfo?.id}_${imageInfo?.secret}.jpg`;
-      // base_image.onload = function(){
-      //     ctx.drawImage(base_image, 0, 0)
-      // }
+      setSize([originalImg.current.naturalWidth, originalImg.current.naturalHeight])
       if (cv) {
         const canvas = canvasRef.current
+        const canvasout = canvasOutput.current
         // @ts-ignore
         const ctxOutput = canvas.getContext('2d')
         const base_image = new Image()
         base_image.src = `${CONSTRUCT_BASE_URL}${imageInfo?.server}/${imageInfo?.id}_${imageInfo?.secret}.jpg`
-        console.log('### 3', base_image.src);
-        const src = cv.imread(base_image)
-        const dst = new cv.Mat()
-        cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0)
-        // You can try more different parameters
-        cv.Canny(base_image, dst, 50, 100, 3, false)
-        cv.imshow('canvasOutput', dst)
-        console.log(src, dst)
-        ctxOutput.drawImage(dst)
-        src.delete()
-        dst.delete()
+        base_image.crossOrigin = "Anonymous";
+        base_image.setAttribute('crossOrigin', '')
+        base_image.onload = function(){
+          ctxOutput.drawImage(base_image, 0, 0)
+          const src = cv.imread(canvas.id)
+          const dst = new cv.Mat()
+          cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0)
+          // You can try more different parameters
+          cv.Canny(src, dst, 50, 100, 3, false)
+          cv.imshow(canvasout.id, dst)
+          // ctxOutput.drawImage(dst)
+          src.delete()
+          dst.delete()
+        }
+
       }
     }
   }, [imageInfo, cv])
 
   const onLoaded = (cvLibrary) => {
-    console.log('Its Loaded', cvLibrary)
-    console.log('### 1', window.cv);
-    setTimeout(function () {
-      console.log('### 2', window.cv);
+    setTimeout(function () {;
       setCv(window.cv)
     }, 500);
   }
 
-  const imgUrl = `${CONSTRUCT_BASE_URL}${imageInfo?.server}/${imageInfo?.id}_${imageInfo?.secret}.jpg`
 
   return (
     <OpenCvProvider
@@ -71,7 +71,7 @@ const ImageEdges = (props) => {
       <div className={'image-edges-wrap'}>
           <h3><a onClick={() => history.goBack()} className={'go-back'}> Back to search results</a></h3>
           <div className="image-card">
-              <img src={imgUrl} id="cvImage" />
+              <img src={imgUrl} id="cvImage" ref={originalImg} />
               {imageInfo && (
                   <div className="photo-info">
                       <p>Title: {imageInfo.title._content}</p>
@@ -83,8 +83,8 @@ const ImageEdges = (props) => {
 
         <div>
           <div className="inputoutput">
-            <canvas ref={canvasRef} />
-            {/*<canvas ref={canvasOutput} />*/}
+            <canvas ref={canvasRef} id={'thecanvasId'} width={size[0]} height={size[1]} style={{display: "none"}}/>
+            <canvas ref={canvasOutput} id={'thecanvasOutId'} width={size[0]} height={size[1]}/>
           </div>
         </div>
       </div>
