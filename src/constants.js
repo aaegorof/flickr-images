726dcfa911b970274e9a1e7d91c11e5496321bc8
Artemiy <aaegorof@gmail.com>
2021-07-09 00:09:55 +0300

    Updates

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const AUTHOR_NAME = '@aaegorof'
-export const AUTHOR_LINK = 'https://github.com/aaegorof'
-
-export const API_KEY = 'ef1f9d4f8ca80dada31c684364355282'
-export const FLICKR_SIG = 'd7f57fa9e01a6a2d6ccd8597b8d2f86b'
-
-export const CONSTRUCT_BASE_URL = 'https://live.staticflickr.com/'
